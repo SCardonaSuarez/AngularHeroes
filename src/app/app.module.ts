c4e568bff2afb89fa4ea859216b001890c6ddd09
Santo <santicardonas777@gmail.com>
2021-07-29 16:50:43 -0500

    Se creo los servicios y se inyecto el HttpClient en listado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
@@ -17,7 +18,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HeroesModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
